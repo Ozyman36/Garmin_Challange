0c9fa608a444edb53b0c8d9f495501aadec7c465
Collen Sibitane <JessySibitane36@gmail.com>
2025-09-19 11:52:50 +0200

    added arguments handling for the uart_decode file

--- uart_decode.cpp
+++ uart_decode.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 #include "iostream"
+#include "vector"
+
 using namespace std;
 
 
 int main (int argc, char** argv){
-    cout<< "we are in main"<<endl;
+    
+    //ensure arguments are correct
+    if (argc < 2){
+        cout<<"Usage: uart_decode <csvfile>\n";
+        return 1;
+    }
+   
+    //firstly need to load the data from the file ( maybe a vector, since unsure of size)
+
 
     return 1;
 
