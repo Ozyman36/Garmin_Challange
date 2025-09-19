847a99b9bc9b7bc9018c8cfac26b4a28bf15f6b5
Collen Sibitane <JessySibitane36@gmail.com>
2025-09-19 12:19:30 +0200

    uart_decode.cpp now opens and loads the data from the csv file, (assuming it has one column for now and <100000 elements)

--- uart_decode.cpp
+++ uart_decode.cpp
@@ -14,8 +14,27 @@ int main (int argc, char** argv){
     }
    
     //firstly need to load the data from the file ( maybe a vector, since unsure of size)
+    ifstream fin(argv[1]); //open file stream
+    if(!fin){cerr<<"Failed to open file."; return 1;} //if file nor found
 
+    vector<double> Voltage; Voltage.reserve(100000);//Vector to store the values from the csv file (maybe reserve space to remove overhead of dynamic allocation)
+    string line;
 
+    while(getline(fin, line)){
+        if(line.empty()) continue; //might be blank lines???
+
+        //csv is comma seperated. (we can assume its only one column? what if other file has more than one column?(can add that feature later))
+
+        //check headers/non numerics and skip
+        try{
+            double val = stod(line); //convert string to double
+            Voltage.push_back(val);
+        } catch (...){
+            //skip headers
+        }
+    }
+
+    cout<<Voltage.size();
     return 1;
 
 }
